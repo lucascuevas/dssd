03e27fe035a5d344dea1aa2ff2011d1a01b22422
Micaela <mika_peralta93@yahoo.com>
2016-10-19 16:08:49 -0300

    redireccion index

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php 
 require_once 'Twig.php';
+
+if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
  TwigController::render_index('index.html');
+}
+else {
+	 header('Location: ' . filter_var('http://' . $_SERVER['HTTP_HOST']. '/listado.php', FILTER_SANITIZE_URL));
+}
 
 
  ?>
