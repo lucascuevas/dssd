a89d9186047d5111a25f08964afacdffd66300ab
Micaela <mika_peralta93@yahoo.com>
2016-10-18 23:28:38 -0300

    paso el token a la vista

--- index.php
+++ index.php
@@ -4,12 +4,14 @@ require_once 'Drive.php';
 require_once 'Twig.php';
 
 $drive= new Drive();
-
+$token=$_SESSION['access_token']['access_token'];
+ 
 if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
   $drive->client()->setAccessToken($_SESSION['access_token']);
   $drive->create_google_service_api();
   $files=$drive->get_list_files();
-  TwigController::render_view('listado.html',$files);
+
+  TwigController::render_view('listado.html',[$files,$token]);
 
 }
 else {
@@ -23,7 +25,7 @@ else {
 	 	$drive->auth($_GET['code']);
 		$drive->create_google_service_api();
 		$files=$drive->get_list_files();
-		TwigController::render_view('listado.html',$files);
+		TwigController::render_view('listado.html',[$files,$token]);
 	}
 }
 
