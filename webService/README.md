7f05baa4ba3bafe1b5496276cca6951d92c293b9
lucascuevas <lucascuevas@live.com.ar>
2016-11-19 16:26:25 -0300

    web service

---
+++ webService/README.md
@@ -0,0 +1,4 @@
+webService
+==========
+
+A Symfony project created on November 7, 2016, 2:19 am.
---
+++ webService/src/AppBundle/Controller/fechayhoraController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+
+
+class fechayhoraController extends Controller
+{
+	/**
+     * @Route("/fecha/{tema}")
+     * @Method({"GET", "POST"})
+     */
+	 public function numberAction($tema)
+    {
+    	$encoders = array(new XmlEncoder(), new JsonEncoder());
+		$normalizers = array(new ObjectNormalizer());
+
+		$serializer = new Serializer($normalizers, $encoders);
+        $number = mt_rand(8, 16);
+        $day = mt_rand(1,30);
+        $month = mt_rand(3,12);
+        $year = 2016;
+        $fecha = $number.'-'.$month.'-'.$year;
+        $hora = array('hora' => $number,'fecha'=> $fecha,'tema' => $tema);
+        $jsonContent = $serializer->serialize($hora, 'json');
+        return new Response(
+           $jsonContent
+        );
+    }
+
+
+
+
+
+}
